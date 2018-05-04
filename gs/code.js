4f73b873eecb62d492d504c1673514495b5d42a0
Simonwep <toports@gmx.de>
2018-05-04 16:48:47 +0200

    Add comments to demo code

--- gs/code.js
+++ gs/code.js
@@ -1,42 +1,44 @@
 const options = {
 
+    // Class which will be appended to selected elements
     class: 'selection',
 
+    // 10px threshold before the selection starts
     startThreshold: 10,
 
+    // All elements in this container can be selected
     containers: ['.box-wrap'],
 
-    selectables: ['.box-wrap'],
-
+    // The container is also the boundary in this case
     boundarys: ['.box-wrap'],
 
     onMove(evt) {
-        const selectedElements = evt.selectedElements;
-        const changedElements = evt.changedElements;
 
+        // Get the currently selected elements and those
+        // which where removed since the last selection.
+        const selectedElements = evt.selectedElements;
+        const removedElements = evt.changedElements.removed;
+
+        // Add a custom class to the elements which where selected.
         for (let se of selectedElements) {
             se.classList.add('selected');
         }
 
-        for (let rm of changedElements.removed) {
+        // Remove the class from elements which where removed 
+        // since the last selection.
+        for (let rm of removedElements) {
             rm.classList.remove('selected');
         }
     },
 
     onStop(evt) {
-        const changedElements = evt.changedElements;
 
-        for (let rm of changedElements.removed) {
+        // Clear selection
+        const elements = [...evt.selectedElements, ...evt.changedElements.removed];
+
+        for (let rm of elements) {
             rm.classList.remove('selected');
         }
-    },
-
-    startFilter(evt) {
-        return true;
-    },
-
-    selectionFilter(evt) {
-        return true;
     },
 };
 
