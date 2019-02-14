aed90c4e2632284f441a5d02d0c9be1462362cf3
Simon <toports@gmx.de>
2019-02-14 15:27:58 +0100

    Simplify demo code

--- gh-page/js/code.js
+++ gh-page/js/code.js
@@ -14,11 +14,11 @@ const selection = Selection.create({
         // Check if clicked element is already selected
         const selected = evt.target.classList.contains('selected');
 
-        // Remove class if the user don't pressed the control key or ⌘ key and the
+        // Remove class if the user isn't pressing the control key or ⌘ key and the
         // current target is already selected
         if (!evt.originalEvent.ctrlKey && !evt.originalEvent.metaKey) {
 
-            // Remove class from every element which is selected
+            // Remove class from every element that is selected
             evt.selectedElements.forEach(s => s.classList.remove('selected'));
 
             // Clear previous selection
@@ -38,13 +38,10 @@ const selection = Selection.create({
         }
     },
 
-    onStart(evt) {
+    onStart({selectedElements, originalEvent}) {
 
-        // Get elements which has been selected so far
-        const selectedElements = evt.selectedElements;
-
-        // Remove class if the user don't pressed the control key or ⌘ key
-        if (!evt.originalEvent.ctrlKey && !evt.originalEvent.metaKey) {
+        // Remove class if the user isn't pressing the control key or ⌘ key
+        if (!originalEvent.ctrlKey && !originalEvent.metaKey) {
 
             // Unselect all elements
             selectedElements.forEach(s => s.classList.remove('selected'));
@@ -54,19 +51,14 @@ const selection = Selection.create({
         }
     },
 
-    onMove(evt) {
+    onMove({selectedElements, changedElements}) {
 
-        // Get the currently selected elements and those
-        // which where removed since the last selection.
-        const selectedElements = evt.selectedElements;
-        const removedElements = evt.changedElements.removed;
-
-        // Add a custom class to the elements which where selected.
+        // Add a custom class to the elements that where selected.
         selectedElements.forEach(s => s.classList.add('selected'));
 
-        // Remove the class from elements which where removed
+        // Remove the class from elements that where removed
         // since the last selection
-        removedElements.forEach(s => s.classList.remove('selected'));
+        changedElements.removed.forEach(s => s.classList.remove('selected'));
     },
 
     onStop() {
