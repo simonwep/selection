5ad1866d5b0bbe1ae5fba3e99c56db741c532750
Simon R <simon@reinisch.io>
2021-06-10 19:00:03 +0200

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,8 @@ module.exports = {
     },
 
     devServer: {
-        dev: {publicPath: '/lib'},
+        devMiddleware: {publicPath: '/lib'},
+        static: '.',
         host: '0.0.0.0',
         port: 3005
     },
