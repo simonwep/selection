cd5689e00f6ae3101f8be3d918a6c9023834715f
Simon R <simon@reinisch.io>
2021-06-12 13:01:43 +0200

    Switch to vite as dev-server

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import {resolve} from 'path';
+import {defineConfig} from 'vite';
+import {version} from './package.json';
+
+export default defineConfig({
+    resolve: {
+        extensions: ['.ts'],
+        alias: {
+            '@utils': resolve('./src/utils')
+        }
+    },
+
+    server: {
+        port: 3005
+    },
+
+    define: {
+        'VERSION': JSON.stringify(version)
+    }
+});
