78371df2a37adc665de37f2c806be39344c99472
Simon R <simon@reinisch.io>
2021-06-17 15:23:28 +0200

    Initial commit

---
+++ main.mjs
@@ -0,0 +1,56 @@
+import SelectionArea from 'https://cdn.jsdelivr.net/npm/@simonwep/selection-js/lib/selection.min.mjs';
+
+const boxes = [
+    ['.boxes.red', 252],
+    ['.boxes.blue', 42],
+    ['.boxes.green', 42]
+];
+
+for (const [sel, items] of boxes) {
+    const container = document.querySelector(sel);
+
+    for (let i = 0; i < items; i++) {
+        container.appendChild(
+            document.createElement('div')
+        );
+    }
+}
+
+// Initialize selectionjs
+const selection = new SelectionArea({
+
+    // All elements in this container can be selected
+    selectables: ['.boxes > div'],
+
+    // The container is also the boundary in this case
+    boundaries: ['.boxes']
+}).on('start', ({store, event}) => {
+
+    // Remove class if the user isn't pressing the control key or ⌘ key
+    if (!event.ctrlKey && !event.metaKey) {
+
+        // Unselect all elements
+        for (const el of store.stored) {
+            el.classList.remove('selected');
+        }
+
+        // Clear previous selection
+        selection.clearSelection();
+    }
+
+}).on('move', ({store: {changed: {added, removed}}}) => {
+
+    // Add a custom class to the elements that where selected.
+    for (const el of added) {
+        el.classList.add('selected');
+    }
+
+    // Remove the class from elements that where removed
+    // since the last selection
+    for (const el of removed) {
+        el.classList.remove('selected');
+    }
+
+}).on('stop', () => {
+    selection.keepSelection();
+});
