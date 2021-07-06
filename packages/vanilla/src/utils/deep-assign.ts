5a2617ec18c669badb94859e8ca54a6ce244fb32
Simon R <simon@reinisch.io>
2021-07-06 18:39:11 +0200

    Fix possible issue with multi-typed options

--- packages/vanilla/src/utils/deep-assign.ts
+++ packages/vanilla/src/utils/deep-assign.ts
@@ -2,13 +2,14 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 export const deepAssign = <O>(target: O, source: any): O => {
     for (const [key, value] of Object.entries(target)) {
+        const sourceValue = source[key];
 
         // Use the default value if there's no value specified
-        target[key as keyof O] = source[key] === undefined ? target[key as keyof O] :
+        target[key as keyof O] = sourceValue === undefined ? target[key as keyof O] :
 
             // Check if it's a nested object and merge if required
-            (typeof value === 'object' && value !== null && !Array.isArray(value)) ?
-                deepAssign(value as O, source[key] as Partial<O>) : source[key];
+            (typeof sourceValue === 'object' && typeof value === 'object' && value !== null && !Array.isArray(value)) ?
+                deepAssign(value as O, sourceValue as Partial<O>) : sourceValue;
     }
 
     return target;
