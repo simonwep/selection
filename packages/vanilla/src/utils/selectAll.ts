4e134f68f7b98cd2314ce3a66daa2b76fce25c0d
Simon Reinisch <simon@reinisch.io>
2022-09-16 17:59:18 +0200

    Fix possible RangeError with lots of elements in selectAll
    Closes #175

--- packages/vanilla/src/utils/selectAll.ts
+++ packages/vanilla/src/utils/selectAll.ts
@@ -8,13 +8,17 @@ export type SelectAllSelectors = readonly (string | Element)[] | string | Elemen
  */
 export function selectAll(selector: SelectAllSelectors, doc: Document = document): Element[] {
     const list = !Array.isArray(selector) ? [selector] : selector;
+    let nodes: Element[] = [];
 
-    const nodes = [];
     for (let i = 0, l = list.length; i < l; i++) {
         const item = list[i];
 
         if (typeof item === 'string') {
-            nodes.push(...Array.from(doc.querySelectorAll(item)));
+            /**
+             * We can't use the spread operator here as with large amounts of elements
+             * we'll get a "Maximum call stack size exceeded"-error.
+             */
+            nodes = nodes.concat(Array.from(doc.querySelectorAll(item)));
         } else if (item instanceof Element) {
             nodes.push(item);
         }
